8f9fb2e3a92a84a13cec9f85ba988624f25e98b7
Katie Tuttle <katka.karolcikova@gmail.com>
2016-10-10 15:58:17 -0700

    display map onlly to logged in users + cluster map

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
   geocoded_by :address
 
- after_validation :geocode
+  after_validation :geocode
+  validates :address, :presence => true
+  validates :name, :presence => true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :places
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
+  resources :places
 
   root :to => 'places#index'
   # root :to => 'home#index'
