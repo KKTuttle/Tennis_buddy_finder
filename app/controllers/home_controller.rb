05e8a7df25dd4c51629a4685e9643e717a8bb602
Katie Tuttle <katka.karolcikova@gmail.com>
2016-10-14 12:45:10 -0700

    add about and contact page and partially working footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,15 @@ class HomeController<ApplicationController
   def index
 
   end
-  
+
+  def show
+    render :about
+  end
+
+  def contact
+    render :contact
+  end
+
 private
   def sign_up_params
     params.require(:user).permit(:email, :avatar, :password, :password_confirmation)
