39f99f922fa8eaee9267115d39e73ce8b3e9baef
Katarina Tuttle <katka.karolcikova@gmail.com>
2016-11-09 21:22:28 -0800

    change maps width

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
     # resources :events
   end
 
-  root :to => 'places#index'
-  # root :to => 'home#index'
+  root :to => 'home#index'
+  # root :to => 'places#index'
+  get "home", to:'places#index'
 
   get 'admin', to: 'places#admin'
   get "about", to: 'home#show'
